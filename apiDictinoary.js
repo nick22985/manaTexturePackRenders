34b2e8875c438e7c510a24ccc59e96479212707f
nick22985 <nick22985@hotmail.com>
2022-11-16 00:17:59 +1000

    Dictionary added

---
+++ apiDictinoary.js
@@ -0,0 +1,645 @@
+let svaDic = {
+	'wildtools-spacesceptre': {
+		static: '/space_crate/supernova_staff.png',
+	},
+	blackholepickaxe: { gif: '/space_crate/blackhole_pickaxe.gif' },
+	spaceshuttle: {
+		static: '/space_crate/space_shuttle.png',
+	},
+	spacexhelmet: {
+		static: '/space_crate/spacex_helmet.png',
+	},
+	moonboots: {
+		static: '/space_crate/moonboots.png',
+	},
+	nebulasword: {
+		gif: '/space_crate/nebula_sword.gif',
+	},
+	shootingstarbow: {
+		static: '/space_crate/shooting_star_bow.png',
+	},
+	cosmicscythe: {
+		static: '/space_crate/cosmic_scythe.png',
+	},
+	galacticaxe: {
+		gif: '/space_crate/nebula_axe.gif',
+	},
+	meteormace: {
+		static: '/space_crate/meteor_mace.png',
+	},
+	hylianshield: {
+		static: '/zelda_crate/hylian_shield.png',
+	},
+	megatonhammer: {
+		static: '/zelda_crate/megaton_hammer.png',
+	},
+	midnashelmet: {
+		static: '/zelda_crate/midna_helmet.png',
+	},
+	demonblade: {
+		static: '/zelda_crate/demon_blade.png',
+	},
+	hyliancrossbow: {
+		static: '/zelda_crate/hylian_crossbow.png',
+	},
+	windwakerwand: {
+		static: '/zelda_crate/wind_waker_wand.png',
+	},
+	hookshot: {
+		static: '/zelda_crate/hookshot.png',
+	},
+	ocarina: {
+		static: '/zelda_crate/ocarina_of_time.png',
+	},
+	kokiritunic: {
+		static: '/zelda_crate/kokiri_tunic.png',
+	},
+	gorontunic: {
+		static: '/zelda_crate/goron_tunic.png',
+	},
+	zoratunic: {
+		static: '/zelda_crate/zora_tunic.png',
+	},
+	supersoaker: {
+		static: '/summer_crate/super_soaker.png',
+	},
+	popsicle: {
+		static: '/summer_crate/popsicle_sword.png',
+	},
+	watermelonpickaxe: {
+		static: '/summer_crate/watermelon_pickaxe.png',
+	},
+	'procosmetics-gadgets-trampoline': {
+		static: '/summer_crate/trampoline.png',
+	},
+	'shades-blue': {
+		static: '/summer_crate/blue_shades.png',
+	},
+	'hawaiianshirt-blue': {
+		static: '/summer_crate/summer_suit_1_chestplate.png',
+	},
+	'swimshorts-blue': {
+		static: '/summer_crate/summer_suit_1_leggings.png',
+	},
+	'flipflops-blue': {
+		static: '/summer_crate/summer_suit_1_boots.png',
+	},
+	'shades-green': {
+		static: '/summer_crate/green_shades.png',
+	},
+	'hawaiianshirt-green': {
+		static: '/summer_crate/summer_suit_3_chestplate.png',
+	},
+	'swimshorts-green': {
+		static: '/summer_crate/summer_suit_3_leggings.png',
+	},
+	'flipflops-green': {
+		static: '/summer_crate/summer_suit_3_boots.png',
+	},
+	'shades-orange': {
+		static: '/summer_crate/orange_shades.png',
+	},
+	'hawaiianshirt-orange': {
+		static: '/summer_crate/summer_suit_4_chestplate.png',
+	},
+	'swimshorts-orange': {
+		static: '/summer_crate/summer_suit_4_leggings.png',
+	},
+	'flipflops-orange': {
+		static: '/summer_crate/summer_suit_4_boots.png',
+	},
+	'shades-red': {
+		static: '/summer_crate/red_shades.png',
+	},
+	'hawaiianshirt-red': {
+		static: '/summer_crate/summer_suit_2_chestplate.png',
+	},
+	'swimshorts-red': {
+		static: '/summer_crate/summer_suit_2_leggings.png',
+	},
+	'flipflops-red': {
+		static: '/summer_crate/summer_suit_2_boots.png',
+	},
+	'shades-yellow': {
+		static: '/summer_crate/yellow_shades.png',
+	},
+	'hawaiianshirt-yellow': {
+		static: '/summer_crate/summer_suit_5_chestplate.png',
+	},
+	'swimshorts-yellow': {
+		static: '/summer_crate/summer_suit_5_leggings.png',
+	},
+	'flipflops-yellow': {
+		static: '/summer_crate/summer_suit_5_boots.png',
+	},
+	mysteryhammer: {
+		static: '/mario_crate/mystery_hammer.png',
+	},
+	magikoopawand: {
+		static: '/mario_crate/magikoopa_wand.png',
+	},
+	superpickaxe: {
+		static: '/mario_crate/super_pickaxe.png',
+	},
+	supermushroom: {
+		static: '/mario_crate/mushroom.png',
+	},
+	propellermushroom: {
+		static: '/mario_crate/propeller.png',
+	},
+	darkbrotherhoodrobes: {
+		static: '/skyrim_crate/dark_brotherhood_robes.png',
+	},
+	dovahkiinhelmet: {
+		static: '/skyrim_crate/dovahkiin_helmet.png',
+	},
+	ancientnordicpickaxe: {
+		static: '/skyrim_crate/nordic_pickaxe.png',
+	},
+	dawnbreaker: {
+		static: '/skyrim_crate/dawnbreaker.png',
+	},
+	daedricwaraxe: {
+		static: '/skyrim_crate/daedric_war_axe.png',
+	},
+	aurielsbow: {
+		static: '/skyrim_crate/auriels_bow.png',
+	},
+	stalhrimshield: {
+		static: '/skyrim_crate/stalhrim_shield.png',
+	},
+	dwarvencrossbow: {
+		static: '/skyrim_crate/dwarven_crossbow.png',
+	},
+	springboots: {
+		static: '', //FIXME: Missing
+	},
+	trowel: {
+		static: '/spring_crate/trowel.png',
+	},
+	'wildtools-blossomstaff': {
+		static: '/spring_crate/blossom_staff.png',
+	},
+	'wildtools-springcleaningwand': {
+		static: '/spring_crate/spring_cleaning_wand.png',
+	},
+	wateringcan: {
+		static: '/spring_crate/watering_can.png',
+	},
+	pineapplehelmet: {
+		static: '/banana_crate/pineapple_helmet.png',
+	},
+	potassiumpickaxe: {
+		static: '/banana_crate/potassium_pickaxe.png',
+	},
+	'wildtools-bananascythe': {
+		static: '/banana_crate/banana_scythe.png',
+	},
+	'wildtools-bananascepter': {
+		static: '/banana_crate/banana_scepter.png',
+	},
+	bananasword: {
+		static: '/banana_crate/banana_sword.png',
+	},
+	valentines22axe: {
+		static: '/valentines22_crate/valentines_axe.png',
+	},
+	valentines22pickaxe: {
+		static: '/valentines22_crate/valentines_pickaxe.png',
+	},
+	valentines22sword: {
+		static: '/valentines22_crate/valentines_sword.png',
+	},
+	valentines22elytra: {
+		static: '/valentines22_crate/valentines_wings.png',
+	},
+	valentines22rod: {
+		static: '/valentines22_crate/valentines_fishing_rod.png',
+	},
+	valentines22bow: {
+		static: '/valentines22_crate/valentines_bow_2.png',
+	},
+	'manafirework-valentines22firework': {
+		static: '', // No texture
+	},
+	'manasets-valentines22-helmet': {
+		static: '/valentines22_crate/valentines_helmet.png',
+	},
+	'manasets-valentines22-chestplate': {
+		static: '/valentines22_crate/valentines_chestplate.png',
+	},
+	'manasets-valentines22-leggings': {
+		static: '/valentines22_crate/valentines_leggings.png',
+	},
+	'manasets-valentines22-boots': {
+		static: '/valentines22_crate/valentines_boots.png',
+	},
+	arctic22wand: {
+		static: '/arctic21_crate/ice_wand.png',
+	},
+	arctic22sword1: {
+		static: '/arctic21_crate/ice_sword_1.png',
+	},
+	arctic22sword2: {
+		static: '/arctic21_crate/ice_sword_2.png',
+	},
+	arctic22pickaxe: {
+		static: '/arctic21_crate/ice_climbing_pickaxe.png',
+	},
+	arctic22elytra: {
+		static: '/arctic21_crate/ice_wings.png',
+	},
+	'manafirework-arctic22firework': {
+		static: '', // No texture
+	},
+	'manasets-arctic22-helmet': {
+		static: '', //FIXME: Missing
+	},
+	'manasets-arctic22-chestplate': {
+		static: '', //FIXME: Missing
+	},
+	'manasets-arctic22-leggings': {
+		static: '', //FIXME: Missing
+	},
+	'manasets-arctic22-boots': {
+		static: '', //FIXME: Missing
+	},
+	christmas21wand: {
+		static: '/christmas21_crate/candy_cane.png',
+	},
+	christmas21sword: {
+		static: '/christmas21_crate/festive_scythe.png',
+	},
+	christmas21sword2: {
+		static: '/christmas21_crate/xmas_sword.png',
+	},
+	christmas21pickaxe: {
+		static: '/christmas21_crate/reindeer_pickaxe.png',
+	},
+	christmas21pickaxe2: {
+		static: '/christmas21_crate/xmas_pickaxe.png', //is a pickaxe
+	},
+	christmas21axe: {
+		static: '/christmas21_crate/xmas_axe.png',
+	},
+	christmas21axe2: {
+		static: '/christmas21_crate/xmas_gloves.png',
+	},
+	christmas21shovel: {
+		static: '/christmas21_crate/xmas_shovel.png',
+	},
+	christmas21bow: {
+		static: '/christmas21_crate/xmas_bow.png',
+	},
+	christmas21shield: {
+		static: '/christmas21_crate/xmas_shield.png',
+	},
+	christmas21helmet: {
+		static: '', //no texture
+	},
+	christmas21chestplate: {
+		static: '', //no texture
+	},
+	christmas21leggings: {
+		static: '', //no texture
+	},
+	christmas21boots: {
+		static: '', //no texture
+	},
+	ocean21sword: {
+		static: '/ocean21_crate/sea_serpent_scythe.png',
+	},
+	ocean21pickaxe: {
+		static: '/ocean21_crate/crab_pickaxe.png',
+	},
+	ocean21helmet: {
+		static: '', //no texture
+	},
+	halloween21axe: {
+		static: '/halloween21_crate/chainsaw_axe.png',
+	},
+	halloween21sword: {
+		static: '/halloween21_crate/jason_machete.png',
+	},
+	halloween21sword2: {
+		static: '/halloween21_crate/jason_machete.png',
+	},
+	halloween21pickaxe: {
+		static: '/halloween21_crate/pumpkin_pickaxe.png',
+	},
+	halloween21wand: {
+		static: '/halloween21_crate/witch_wand.png',
+	},
+	halloween21chestplate: {
+		static: '', //no texture
+	},
+	halloween21leggings: {
+		static: '', //no texture
+	},
+	anime21axe: {
+		static: '/anime21_crate/divine_axe.png',
+	},
+	anime21sword: {
+		static: '/anime21_crate/yoru.png',
+	},
+	anime21bow: {
+		static: '/anime21_crate/fate_bow.png',
+	},
+	'wildtools-anime21pickaxe': {
+		static: '/anime21_crate/blaze_reap.png',
+	},
+	anime21helmet: {
+		static: '', //no texture
+	},
+	anime21chestplate: {
+		static: '', //no texture
+	},
+	spongebob21sword: {
+		static: '/spongebob21_crate/spatula.png',
+	},
+	spongebob21trident: {
+		static: '/spongebob21_crate/king_neptune_trident.png',
+	},
+	spongebob21gun: {
+		static: '/spongebob21_crate/sandys_pop_gun.png',
+	},
+	spongebob21boots: {
+		static: '', //no texture
+	},
+	spongebob21leggings: {
+		static: '', //no texture
+	},
+	SortingHat: {
+		static: '',
+	},
+	harrypotter21wand: {
+		static: '/harrypotter21_crate/elder_wand.png',
+	},
+	harrypotter21sword: {
+		static: '/harrypotter21_crate/gordic_sword.png',
+	},
+	harrypotter21axe: {
+		static: '/harrypotter21_crate/executioners_axe.png',
+	},
+	harrypotter21chestplate: {
+		static: '', //no texture
+	},
+	bopeepsword: {
+		static: '/toystory21_crate/bo_peep_staff.png',
+	},
+	prospectorpickaxe: {
+		static: '/toystory21_crate/prospector_pickaxe.png',
+	},
+	'manafirework-thebigone': {
+		static: '/toystory21_crate/the_big_one.png', //no texture
+	},
+	'mana-zurgblaster': {
+		static: '/toystory21_crate/zurg_blaster.png',
+	},
+	'mv-bopeepstaff': {
+		static: '/toystory21_crate/bo_peep_staff.png',
+	},
+	'mv-prospectorpickaxe': {
+		static: '/toystory21_crate/prospector_pickaxe.png',
+	},
+	'procosmetics-gadgets-party-popper': {
+		static: '/toystory21_crate/the_big_one.png', //no texture
+	},
+	zurgblaster: {
+		static: '/toystory21_crate/zurg_blaster.png',
+	},
+	charizardbow: {
+		static: '/pokemon21_crate/charizard_bow1.png',
+	},
+	keaxe: {
+		static: '/pokemon21_crate/kyogre_axe.png',
+	},
+	excadrillpickaxe: {
+		static: '/pokemon21_crate/excadrill_pickaxe.png',
+	},
+	groudonsword: {
+		static: '/pokemon21_crate/groudon_sword.png',
+	},
+	'mv-charizardbow': {
+		static: '/pokemon21_crate/charizard_bow1.png',
+	},
+	'mv-kyogreaxe': {
+		static: '/pokemon21_crate/kyogre_axe.png',
+	},
+	'mv-groudonsword': {
+		static: '/pokemon21_crate/groudon_sword.png',
+	},
+	'mv-excadrillpickaxe': {
+		static: '/pokemon21_crate/excadrill_pickaxe.png',
+	},
+	'procosmetics-gadgets-grappling-hook': {
+		static: '/pokemon21_crate/pokeball.png',
+	},
+	'mv-chocolatesword': {
+		static: '/easter21_crate/choco_sword.png',
+	},
+	'mv-pockystick': {
+		static: '/easter21_crate/pocky_rod.png',
+	},
+	'mv-rabbitfeet': {
+		static: '', //no texture
+	},
+	'mv-silverspoon': {
+		static: '/easter21_crate/silver_spoon_shovel.png',
+	},
+	'procosmetics-gadgets-paintball': {
+		static: '', //no texture
+	},
+	ChocolateBarSword: {
+		static: '/easter21_crate/choco_sword.png',
+	},
+	pockyrod: {
+		static: '/easter21_crate/pocky_rod.png',
+	},
+	SilverSpoon: {
+		static: '/easter21_crate/silver_spoon_shovel.png',
+	},
+	'mv-shamrockaxe-21': {
+		static: '/stpatrick21_crate/axe.png',
+	},
+	'mv-shamrockchestplate-21': {
+		static: '/stpatrick21_crate/chestplate.png',
+	},
+	'mv-shamrockelytra-21': {
+		static: '/stpatrick21_crate/wings.png',
+	},
+	'mv-shamrockleggings-21': {
+		static: '/stpatrick21_crate/leggings.png',
+	},
+	'mv-shamrockpickaxe-21': {
+		static: '/stpatrick21_crate/pickaxe.png',
+	},
+	'mv-shamrocksword-21': {
+		static: '/stpatrick21_crate/sword.png',
+	},
+	'procosmetics-gadgets-coin-party-bomb': {
+		static: '', //no texture
+	},
+	'wocketpickaxe-21': {
+		static: '/drseuss21_crate/wocketpickaxe.png',
+	},
+	'blindfoldedbowman-21': {
+		static: '/drseuss21_crate/plain_gold_bow.png',
+	},
+	'kingbirtramsstilt-21': {
+		static: '/drseuss21_crate/king_birtrams_stilt.png',
+	},
+	palegreenpants: {
+		static: '/drseuss21_crate/pale_green_pants.png',
+	},
+	'mv-dragonchestplate-21': {
+		static: '', //no texture
+	},
+	'mv-dragonaxe-21': {
+		static: '/chinese21_crate/dragon_axe.png',
+	},
+	'mv-dragonhelmet-21': {
+		static: '',
+	},
+	'mv-dragonrod-21': {
+		static: '', //no texture
+	},
+	'mv-padao-21': {
+		static: '', //no texture
+	},
+	'mv-dragonpickaxe-21': {
+		static: '/chinese21_crate/chinese_fortune_dao.png',
+	},
+	'dragonaxe-21': {
+		static: '/chinese21_crate/dragon_axe.png',
+	},
+	'manasets-dragon-helmet': {
+		static: '',
+	},
+	'manasets-dragon-chestplate': {
+		static: '',
+	},
+	'manasets-dragon-leggings': {
+		static: '',
+	},
+	'manasets-dragon-boots': {
+		static: '',
+	},
+	teddybear: {
+		static: '/valentines20_crate/teddy_bear.png',
+	},
+	erosbow: {
+		static: '/valentines20_crate/bow_draw2.png',
+	},
+	axeoflove: {
+		static: '/valentines20_crate/staff.png',
+	},
+	harp: {
+		static: '/valentines20_crate/harp.png',
+	},
+	'mv-erosboots': {
+		static: '',
+	},
+	'mv-erosleggings': {
+		static: '',
+	},
+	'mv-erosbow': {
+		static: '/valentines20_crate/bow_draw2.png',
+	},
+	'mv-axeoflove': {
+		static: '/valentines20_crate/staff.png',
+	},
+	'procosmetics-gadgets-merry-go-round': {
+		static: '',
+	},
+	'sparkleaxe-21': {
+		gif: '/newyear20_crate/axe.gif',
+	},
+	'sparklespickaxe-21': {
+		gif: '/newyear20_crate/pickaxe.gif',
+	},
+	'sparkleshovel-21': {
+		gif: '/newyear20_crate/shovel.gif',
+	},
+	'sparklesword-21': {
+		gif: '/newyear20_crate/sword.gif',
+	},
+	'goldenelytra-21': {
+		gif: '/newyear20_crate/elytra.gif',
+	},
+	'manafirework-nye': {
+		static: '', //no texture
+	},
+	'mv-sparkleaxe-21': {
+		gif: '/newyear20_crate/axe.gif',
+	},
+	'mv-sparklechestplate-21': {
+		gif: '/newyear20_crate/chestplate.gif',
+	},
+	'mv-sparkleshovel-21': {
+		gif: '/newyear20_crate/shovel.gif',
+	},
+	'mv-sparklespickaxe-21': {
+		gif: '/newyear20_crate/pickaxe.gif',
+	},
+	'mv-sparklesword-21': {
+		gif: '/newyear20_crate/sword.gif',
+	},
+	'mv-goldenelytra-21': {
+		gif: '/newyear20_crate/elytra.gif',
+	},
+	'procosmetics-gadgets-disco-ball': {
+		gif: '/newyear20_crate/discoball.gif',
+	},
+	'mv-gingerbreadpickaxe': {
+		static: '/christmas20_crate/gingerpickaxe.png',
+	},
+	'mv-frostbitebow': {
+		static: '/christmas20_crate/frostbow.png',
+	},
+	'mv-frostbitesword': {
+		static: '/christmas20_crate/frostsword.png',
+	},
+	santasstockings: {
+		static: '/christmas20_crate/santasstockings.png',
+	},
+	blizzardbow: {
+		static: '/pokemon21_crate/charizard_bow1.png',
+	},
+	'cyberaxe-20': {
+		static: '/cybermonday_crate/cyberaxe.png',
+	},
+	'cybershovel-20': {
+		static: '/cybermonday_crate/cybershovel.png',
+	},
+	'fork-20': {
+		static: '/thanksgiving20_crate/fork.png',
+	},
+	'wildtools-turkey_leg_limited': {
+		static: '/thanksgiving20_crate/turkeyleg.png',
+	},
+	'manasets-mv-thanksgiving20-helmet': {
+		static: '', //no texture
+	},
+	'manasets-mv-thanksgiving20-chestplate': {
+		static: '', //no texture
+	},
+	'manasets-mv-thanksgiving20-leggings': {
+		static: '', //no texture
+	},
+	'manasets-mv-thanksgiving20-boots': {
+		static: '', //no texture
+	},
+	froggerboots: {
+		static: '/arcade_crate/froggerboots.png',
+	},
+	CapeFeather: {
+		static: '/arcade_crate/capefeather.png',
+	},
+	'manaspaceblaster': {
+		static: '', //no texture
+	},
+	'scorpionsword': {
+		static: '/arcade_crate/scorpionsword.png',
+	}
+}
